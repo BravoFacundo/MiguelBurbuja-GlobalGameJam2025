8f540927e1cc8a8aa4424b779a7f43eeaa1038ff
germandifrancesco <germandifrancesco@gmail.com>
2025-01-28 19:49:29 -0300

    wind directions and force

--- Assets/_Scripts/Wind.cs
+++ Assets/_Scripts/Wind.cs
@@ -5,4 +5,33 @@ using UnityEngine;
 public class Wind : MonoBehaviour
 {
     public Vector2 windDirection;
+    public float windStrength = 10f;
+void Update()
+    {
+        float zRotation = transform.rotation.eulerAngles.z;
+        if (zRotation == 0)
+        {
+            windDirection = new Vector2(0, windStrength);
+
+        }
+        else if (zRotation == 90)
+        {
+            windDirection = new Vector2(windStrength, 0);
+
+        }
+        else if (zRotation == 180)
+        {
+            windDirection = new Vector2(0,-windStrength);
+
+        }
+        else if (zRotation == 270)
+        {
+            windDirection = new Vector2(-windStrength, 0);
+
+        }
+        else
+        {
+            windDirection = Vector2.zero; // Default case if the rotation is not exactly 0, 90, 180, or 270
+        }
+    }
 }

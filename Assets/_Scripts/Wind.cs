b5d898793ef4ffb4d2446e9abf8e69bc4f264855
Facundo Bravo <facundobravot@gmail.com>
2025-02-08 19:12:57 -0300

    Fix Wind

--- Assets/_Scripts/Wind.cs
+++ Assets/_Scripts/Wind.cs
@@ -6,32 +6,15 @@ public class Wind : MonoBehaviour
 {
     public Vector2 windDirection;
     public float windStrength = 10f;
-void Update()
+
+    private void Start()
     {
-        float zRotation = transform.rotation.eulerAngles.z;
-        if (zRotation == 0)
-        {
-            windDirection = new Vector2(0, windStrength);
-
-        }
-        else if (zRotation == 90)
-        {
-            windDirection = new Vector2(windStrength, 0);
-
-        }
-        else if (zRotation == 180)
-        {
-            windDirection = new Vector2(0,-windStrength);
-
-        }
-        else if (zRotation == 270)
-        {
-            windDirection = new Vector2(-windStrength, 0);
-
-        }
-        else
-        {
-            windDirection = Vector2.zero; // Default case if the rotation is not exactly 0, 90, 180, or 270
-        }
+        UpdateWindDirection();
     }
+    private void UpdateWindDirection()
+    {
+        Vector3 worldDirection = transform.up;
+        windDirection = new Vector2(worldDirection.x, worldDirection.y).normalized;
+    }
+
 }
